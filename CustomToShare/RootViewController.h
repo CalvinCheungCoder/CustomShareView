5c30d5294396ddd42cdd7c393b71f5c194bfec57
zhangdinghao <zhangdinghao@zhangdinghaodeMacBook-Pro.local>
2016-09-17 21:57:49 +0800

    自定义分享视图，包含微博、微信、QQ等主流分享界面

---
+++ CustomToShare/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  CustomToShare
+//
+//  Created by 张丁豪 on 16/9/17.
+//  Copyright © 2016年 张丁豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
